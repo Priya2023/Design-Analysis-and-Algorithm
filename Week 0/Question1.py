2b80014fe0d73c5314de532dbd5bd0e9cb924456
Priya2023 <87319597+Priya2023@users.noreply.github.com>
2021-09-22 18:50:16 +0530

    hello new code

--- Week 0/Question1.py
+++ Week 0/Question1.py
@@ -13,10 +13,10 @@ def searchInArray(array, value):
 
 if __name__=="__main__":
     arr = []
-    n = int(input("Enter size : "))
+    n = int(input("Enter Element size : "))
     for i in range(0,n):
         arr.append(int(input("Enter element : ")))
 
-    num = int(input("Enter Element for Search : "))
+    num = int(input("Enter Element Search : "))
     print(searchInArray(arr, num))
 
